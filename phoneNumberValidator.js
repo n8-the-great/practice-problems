4765216c4b177660d0e1e130ec37df30faec985c
Nate Rana <n8.rana@gmail.com>
2023-10-02 19:28:45 -0500

    phoneNumberValidator.js added tests

--- phoneNumberValidator.js
+++ phoneNumberValidator.js
@@ -62,8 +62,37 @@ function telephoneCheck(str) {
 
 
 
-var isValid;
-isValid = telephoneCheck("1-555-535-5755");
+console.log('1 555-555-5555 should be true?: ' + telephoneCheck("1 555-555-5555"));
+console.log('1 (555) 555-5555 should be true?: ' + telephoneCheck("1 (555) 555-5555"));
+console.log('5555555555 should be true?: ' + telephoneCheck("5555555555"));
+console.log('555-555-5555 should be true?: ' + telephoneCheck("555-555-5555"));
+console.log('(555)555-5555 should be true?: ' + telephoneCheck("(555)555-5555"));
+console.log('1(555)555-5555 should be true?: ' + telephoneCheck("1(555)555-5555"));
+console.log('1 456 789 4444 should be true?: ' + telephoneCheck("1 456 789 4444"));
 
+
+console.log('\n555-5555 should be false?: ' + telephoneCheck("555-5555"));
+console.log('5555555 should be false?: ' + telephoneCheck("5555555"));
+console.log('1 555)555-5555 should be false?: ' + telephoneCheck("1 555)555-5555"));
+console.log('123**&!!asdf# should be false?: ' + telephoneCheck("123**&!!asdf#"));
+console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
+console.log('(6054756961) should be false?: ' + telephoneCheck("(6054756961)"));
+console.log('2 (757) 622-7382 should be false?: ' + telephoneCheck("2 (757) 622-7382"));
+console.log('10 (757) 622-7382 should be false?: ' + telephoneCheck("10 (757) 622-7382"));
+console.log('0 (757) 622-7382 should be false?: ' + telephoneCheck("0 (757) 622-7382"));
+console.log('2 757 622-7382 should be false?: ' + telephoneCheck("2 757 622-7382"));
+console.log('-1 (757) 622-7382 should be false?: ' + telephoneCheck("-1 (757) 622-7382"));
+console.log('27576227382 should be false?: ' + telephoneCheck("27576227382"));
+console.log('(275)76227382 should be false?: ' + telephoneCheck("(275)76227382"));
+console.log('2(757)6227382 should be false?: ' + telephoneCheck("2(757)6227382"));
+console.log('2(757)622-7382 should be false?: ' + telephoneCheck("2(757)622-7382"));
+console.log('555)-555-5555 should be false?: ' + telephoneCheck("555)-555-5555"));
+console.log('(555-555-5555 should be false?: ' + telephoneCheck("(555-555-5555"));
+console.log('(555)5(55?)-5555 should be false?: ' + telephoneCheck("(555)5(55?)-5555"));
+console.log('55 55-55-555-5 should be false?: ' + telephoneCheck("55 55-55-555-5"));
+console.log('11 555-555-5555 should be false?: ' + telephoneCheck("11 555-555-5555"));
+// console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
+// console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
+// console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
+// console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
 // isValid = telephoneCheck("3s");
-console.log(isValid);
