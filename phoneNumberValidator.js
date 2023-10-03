eddf4531d91ca145fbc71b1b14adbaf5a8e8a4b6
Nate Rana <n8.rana@gmail.com>
2023-10-02 20:04:27 -0500

    phoneNumberValidator.js unit tests added, central office and line regex tested and so far working

--- phoneNumberValidator.js
+++ phoneNumberValidator.js
@@ -27,11 +27,13 @@ function telephoneCheck(str) {
     return false;
   }
 
-  // create a substring of the last 6 and check for 1 + 4 digits, possibly separated or not. if not 4 digits, return false
-  let lineCode = str.substring(str.length-6, str.length);
-  let regexLineCode = /^(\d{1})([- \d]?)(\d{4})$/;
-  console.log(lineCode);
-  if (!regexLineCode.test(lineCode)) {
+  // create a substring of the central office + line code, possibly separated or not.
+  // might have part of area code, so regex includes that
+  // if not valid, return false
+  let colinecode = str.substring(str.length-8, str.length);
+  let regexLineCode = /^([-) \d]?)(\d{3})([- \d])?(\d{4})$/;
+  console.log(colinecode);
+  if (!regexLineCode.test(colinecode)) {
     return false;
   }
 
@@ -61,36 +63,62 @@ function telephoneCheck(str) {
 }
 
 
+let test1 = '(452)2345678';
+let test2 = '(322)215-3678';
+let test3 = '(452) 234-5678';
+let test4 = '(322) 234 5678';
 
-console.log('1 555-555-5555 should be true?: ' + telephoneCheck("1 555-555-5555"));
-console.log('1 (555) 555-5555 should be true?: ' + telephoneCheck("1 (555) 555-5555"));
-console.log('5555555555 should be true?: ' + telephoneCheck("5555555555"));
-console.log('555-555-5555 should be true?: ' + telephoneCheck("555-555-5555"));
-console.log('(555)555-5555 should be true?: ' + telephoneCheck("(555)555-5555"));
-console.log('1(555)555-5555 should be true?: ' + telephoneCheck("1(555)555-5555"));
-console.log('1 456 789 4444 should be true?: ' + telephoneCheck("1 456 789 4444"));
+console.log(`${test1} shoud be T: ${telephoneCheck(test1)}`);
+console.log(`${test2} should be T: ${telephoneCheck(test2)}`);
+console.log(`${test3} shoud be T: ${telephoneCheck(test3)}`);
+console.log(`${test4} should be T: ${telephoneCheck(test4)}`);
 
 
-console.log('\n555-5555 should be false?: ' + telephoneCheck("555-5555"));
-console.log('5555555 should be false?: ' + telephoneCheck("5555555"));
-console.log('1 555)555-5555 should be false?: ' + telephoneCheck("1 555)555-5555"));
-console.log('123**&!!asdf# should be false?: ' + telephoneCheck("123**&!!asdf#"));
-console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
-console.log('(6054756961) should be false?: ' + telephoneCheck("(6054756961)"));
-console.log('2 (757) 622-7382 should be false?: ' + telephoneCheck("2 (757) 622-7382"));
-console.log('10 (757) 622-7382 should be false?: ' + telephoneCheck("10 (757) 622-7382"));
-console.log('0 (757) 622-7382 should be false?: ' + telephoneCheck("0 (757) 622-7382"));
-console.log('2 757 622-7382 should be false?: ' + telephoneCheck("2 757 622-7382"));
-console.log('-1 (757) 622-7382 should be false?: ' + telephoneCheck("-1 (757) 622-7382"));
-console.log('27576227382 should be false?: ' + telephoneCheck("27576227382"));
-console.log('(275)76227382 should be false?: ' + telephoneCheck("(275)76227382"));
-console.log('2(757)6227382 should be false?: ' + telephoneCheck("2(757)6227382"));
-console.log('2(757)622-7382 should be false?: ' + telephoneCheck("2(757)622-7382"));
-console.log('555)-555-5555 should be false?: ' + telephoneCheck("555)-555-5555"));
-console.log('(555-555-5555 should be false?: ' + telephoneCheck("(555-555-5555"));
-console.log('(555)5(55?)-5555 should be false?: ' + telephoneCheck("(555)5(55?)-5555"));
-console.log('55 55-55-555-5 should be false?: ' + telephoneCheck("55 55-55-555-5"));
-console.log('11 555-555-5555 should be false?: ' + telephoneCheck("11 555-555-5555"));
+test1 = '(452)234+5678';
+test2 = '(322) 215678';
+test3 = '(452) 234x5678';
+test4 = '(322)234  5678';
+
+console.log(`${test1} shoud be F: ${telephoneCheck(test1)}`);
+console.log(`${test2} should be F: ${telephoneCheck(test2)}`);
+console.log(`${test3} shoud be F: ${telephoneCheck(test3)}`);
+console.log(`${test4} should be F: ${telephoneCheck(test4)}`);
+
+
+
+// final test suite
+// console.log('1 555-555-5555 should be true?: ' + telephoneCheck("1 555-555-5555"));
+// console.log('1 (555) 555-5555 should be true?: ' + telephoneCheck("1 (555) 555-5555"));
+// console.log('5555555555 should be true?: ' + telephoneCheck("5555555555"));
+// console.log('555-555-5555 should be true?: ' + telephoneCheck("555-555-5555"));
+// console.log('(555)555-5555 should be true?: ' + telephoneCheck("(555)555-5555"));
+// console.log('1(555)555-5555 should be true?: ' + telephoneCheck("1(555)555-5555"));
+// console.log('1 456 789 4444 should be true?: ' + telephoneCheck("1 456 789 4444"));
+
+
+// console.log('\n555-5555 should be false?: ' + telephoneCheck("555-5555"));
+// console.log('5555555 should be false?: ' + telephoneCheck("5555555"));
+// console.log('1 555)555-5555 should be false?: ' + telephoneCheck("1 555)555-5555"));
+// console.log('123**&!!asdf# should be false?: ' + telephoneCheck("123**&!!asdf#"));
+// console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
+// console.log('(6054756961) should be false?: ' + telephoneCheck("(6054756961)"));
+// console.log('2 (757) 622-7382 should be false?: ' + telephoneCheck("2 (757) 622-7382"));
+// console.log('10 (757) 622-7382 should be false?: ' + telephoneCheck("10 (757) 622-7382"));
+// console.log('0 (757) 622-7382 should be false?: ' + telephoneCheck("0 (757) 622-7382"));
+// console.log('2 757 622-7382 should be false?: ' + telephoneCheck("2 757 622-7382"));
+// console.log('-1 (757) 622-7382 should be false?: ' + telephoneCheck("-1 (757) 622-7382"));
+// console.log('27576227382 should be false?: ' + telephoneCheck("27576227382"));
+// console.log('(275)76227382 should be false?: ' + telephoneCheck("(275)76227382"));
+// console.log('2(757)6227382 should be false?: ' + telephoneCheck("2(757)6227382"));
+// console.log('2(757)622-7382 should be false?: ' + telephoneCheck("2(757)622-7382"));
+// console.log('555)-555-5555 should be false?: ' + telephoneCheck("555)-555-5555"));
+// console.log('(555-555-5555 should be false?: ' + telephoneCheck("(555-555-5555"));
+// console.log('(555)5(55?)-5555 should be false?: ' + telephoneCheck("(555)5(55?)-5555"));
+// console.log('55 55-55-555-5 should be false?: ' + telephoneCheck("55 55-55-555-5"));
+// console.log('11 555-555-5555 should be false?: ' + telephoneCheck("11 555-555-5555"));
+
+
+
 // console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
 // console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
 // console.log('55555555 should be false?: ' + telephoneCheck("55555555"));
